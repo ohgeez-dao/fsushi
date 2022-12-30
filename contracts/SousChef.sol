1d6f38b687d4f3346d98f7d65289e92320b7853c
LevX <team@levx.io>
2022-12-31 01:22:00 +0900

    Separate FSushiBill(ERC20) from SousChef

--- contracts/SousChef.sol
+++ contracts/SousChef.sol
@@ -2,18 +2,16 @@
 
 pragma solidity ^0.8.17;
 
+import "@openzeppelin/contracts/proxy/Clones.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeCast.sol";
 import "@openzeppelin/contracts/utils/math/Math.sol";
 import "./interfaces/ISousChef.sol";
-import "./interfaces/IFSushiVault.sol";
-import "./interfaces/IFSushiKitchen.sol";
 import "./interfaces/IFSushi.sol";
 import "./interfaces/IFlashStrategySushiSwapFactory.sol";
 import "./interfaces/IFlashStrategySushiSwap.sol";
-import "./libraries/DateUtils.sol";
+import "./FSushiBill.sol";
 
 contract SousChef is Ownable, ISousChef {
     using SafeERC20 for IERC20;
@@ -21,14 +19,14 @@ contract SousChef is Ownable, ISousChef {
     using SafeCast for int256;
     using DateUtils for uint256;
 
-    uint256 internal constant TOKENLESS_PRODUCTION = 40;
-
     uint256 public constant override BONUS_MULTIPLIER = 10;
     uint256 public constant override REWARDS_FOR_INITIAL_WEEK = BONUS_MULTIPLIER * 30000e18;
 
     address public immutable override fSushi;
     address public immutable override flashStrategyFactory;
     uint256 public immutable override startWeek;
+
+    address internal immutable _implementation;
 
     /**
      * @notice address of IFSushiVault
@@ -40,6 +38,7 @@ contract SousChef is Ownable, ISousChef {
      */
     address public override kitchen;
 
+    mapping(uint256 => address) public override getFSushiBill;
     /**
      * @notice how much rewards to be minted at the week
      */
@@ -47,37 +46,7 @@ contract SousChef is Ownable, ISousChef {
     /**
      * @notice weeklyRewards is guaranteed to be correct before this week
      */
-    uint256 internal _lastCheckpoint; // week
-
-    mapping(uint256 => uint256) public override totalSupply; // pid => amount
-    mapping(uint256 => uint256) public override workingSupply; // pid => amount
-    /**
-     * @notice points = ∫W(t)dt, where W(t) is the working supply at the week
-     */
-    mapping(uint256 => mapping(uint256 => uint256)) public override points; // pid => week => points
-    /**
-     * @notice points of pid is guaranteed to be correct before this time's week start
-     */
-    mapping(uint256 => uint256) public override lastCheckpoint; // pid => timestamp
-
-    mapping(uint256 => mapping(address => uint256)) public override balanceOf; // pid => account => amount
-    mapping(uint256 => mapping(address => uint256)) public override workingBalanceOf; // pid => account => amount
-    /**
-     * @notice userPoints = ∫w(t)dt, where a(t) is the working balance of account at the week
-     */
-    mapping(uint256 => mapping(address => mapping(uint256 => uint256))) public override userPoints; // pid => account => week => points
-    /**
-     * @notice userPoints of account is guaranteed to be correct before this week
-     */
-    mapping(uint256 => mapping(address => uint256)) public override userLastCheckpoint; // pid => account => timestamp
-    /**
-     * @notice how much rewards were claimed in total for account in pid
-     */
-    mapping(uint256 => mapping(address => uint256)) public override claimedRewards; // pid => account => amount
-    /**
-     * @notice in the next claim, rewards will be accumulated from this week
-     */
-    mapping(uint256 => mapping(address => uint256)) public override nextClaimableWeek; // pid => account => week
+    uint256 public override lastCheckpoint; // week
 
     constructor(
         address _fSushi,
@@ -91,8 +60,16 @@ contract SousChef is Ownable, ISousChef {
         flashStrategyFactory = _flashStrategyFactory;
         uint256 week = block.timestamp.toWeekNumber() + 1;
         startWeek = week;
-        _lastCheckpoint = week;
+        lastCheckpoint = week;
         weeklyRewards[week] = REWARDS_FOR_INITIAL_WEEK;
+
+        FSushiBill bill = new FSushiBill();
+        bill.initialize(type(uint256).max, address(0));
+        _implementation = address(bill);
+    }
+
+    function predictFSushiBillAddress(uint256 pid) external view override returns (address bill) {
+        bill = Clones.predictDeterministicAddress(_implementation, bytes32(pid));
     }
 
     function updateFSushiVault(address _fSushiVault) external override onlyOwner {
@@ -111,61 +88,22 @@ contract SousChef is Ownable, ISousChef {
         emit UpdateFSushiKitchen(_fSushiKitchen);
     }
 
-    function deposit(
-        uint256 pid,
-        uint256 amount,
-        address beneficiary
-    ) external override {
-        if (block.timestamp.toWeekNumber() < startWeek) revert TooEarly();
+    function createFSushiBill(uint256 pid) external override returns (address bill) {
+        if (getFSushiBill[pid] != address(0)) revert FSushiBillCreated();
 
         address strategy = IFlashStrategySushiSwapFactory(flashStrategyFactory).getFlashStrategySushiSwap(pid);
         if (strategy == address(0)) revert InvalidPid();
 
-        if (amount > 0) {
-            address fToken = IFlashStrategySushiSwap(strategy).fToken();
-            IERC20(fToken).safeTransferFrom(msg.sender, address(this), amount);
+        bill = Clones.cloneDeterministic(_implementation, bytes32(pid));
+        FSushiBill(bill).initialize(pid, IFlashStrategySushiSwap(strategy).fToken());
 
-            _userCheckpoint(pid, msg.sender);
+        getFSushiBill[pid] = bill;
 
-            uint256 _balance = balanceOf[pid][msg.sender] + amount;
-            balanceOf[pid][msg.sender] = _balance;
-            uint256 _totalSupply = totalSupply[pid] + amount;
-            totalSupply[pid] = _totalSupply;
-
-            _updateWorkingBalance(pid, msg.sender, _balance, _totalSupply);
-        }
-
-        emit Deposit(pid, amount, beneficiary);
+        emit CreateFSushiBill(pid, bill);
     }
 
-    function withdraw(
-        uint256 pid,
-        uint256 amount,
-        address beneficiary
-    ) external override {
-        address strategy = IFlashStrategySushiSwapFactory(flashStrategyFactory).getFlashStrategySushiSwap(pid);
-        if (strategy == address(0)) revert InvalidPid();
-
-        if (amount > 0) {
-            uint256 _balance = balanceOf[pid][msg.sender] - amount;
-            balanceOf[pid][msg.sender] = _balance;
-            uint256 _totalSupply = totalSupply[pid] - amount;
-            totalSupply[pid] = _totalSupply;
-
-            _updateWorkingBalance(pid, msg.sender, _balance, _totalSupply);
-
-            address fToken = IFlashStrategySushiSwap(strategy).fToken();
-            IERC20(fToken).safeTransfer(beneficiary, amount);
-        }
-
-        emit Withdraw(pid, amount, beneficiary);
-    }
-
-    /**
-     * @dev if this function doesn't get called for 512 weeks (around 9.8 years) this contract breaks
-     */
-    function checkpoint(uint256 pid) public override {
-        uint256 from = _lastCheckpoint + 1;
+    function checkpoint() external override {
+        uint256 from = lastCheckpoint + 1;
         uint256 until = block.timestamp.toWeekNumber();
         for (uint256 i; i < 512; ) {
             uint256 week = from + i;
@@ -186,132 +124,16 @@ contract SousChef is Ownable, ISousChef {
                 ++i;
             }
         }
-        _lastCheckpoint = until;
-
-        uint256 prevCheckpoint = lastCheckpoint[pid];
-        _updatePoints(points[pid], workingSupply[pid], prevCheckpoint);
-        if (prevCheckpoint < block.timestamp) {
-            lastCheckpoint[pid] = block.timestamp;
-        }
-
-        emit Checkpoint(pid);
+        lastCheckpoint = until;
     }
 
-    function userCheckpoint(uint256 pid, address account) external override {
-        _userCheckpoint(pid, account);
-        _updateWorkingBalance(pid, account, balanceOf[pid][account], totalSupply[pid]);
-    }
-
-    function _userCheckpoint(uint256 pid, address account) internal {
-        checkpoint(pid);
-
-        uint256 prevCheckpoint = userLastCheckpoint[pid][account];
-        _updatePoints(userPoints[pid][account], workingBalanceOf[pid][account], prevCheckpoint);
-        if (prevCheckpoint < block.timestamp) {
-            userLastCheckpoint[pid][account] = block.timestamp;
-        }
-
-        emit UserCheckpoint(pid, account);
-    }
-
-    function _updatePoints(
-        mapping(uint256 => uint256) storage _points,
-        uint256 workingBalance,
-        uint256 lastTime
-    ) internal {
-        if (workingBalance == 0) return;
-
-        if (lastTime == 0) {
-            lastTime = startWeek.toTimestamp();
-        }
-
-        uint256 from = lastTime.toWeekNumber();
-        for (uint256 i; i < 512; ) {
-            uint256 week = from + i;
-            uint256 weekStart = week.toTimestamp();
-            uint256 weekEnd = weekStart + WEEK;
-            if (block.timestamp <= weekStart) break;
-            if (block.timestamp < weekEnd) {
-                _points[week] += workingBalance * (block.timestamp - Math.max(lastTime, weekStart));
-                break;
-            }
-            if (i == 0) {
-                _points[week] += workingBalance * (weekEnd - lastTime);
-            } else {
-                _points[week] += workingBalance * WEEK;
-            }
-
-            unchecked {
-                ++i;
-            }
-        }
-    }
-
-    function _updateWorkingBalance(
+    function mintFSushi(
         uint256 pid,
-        address account,
-        uint256 balance,
-        uint256 supply
-    ) internal {
-        address _vault = vault;
-        IFSushiVault(_vault).userCheckpoint(account);
+        address to,
+        uint256 amount
+    ) external override {
+        if (getFSushiBill[pid] != msg.sender) revert Forbidden();
 
-        uint256 week = block.timestamp.toWeekNumber();
-        uint256 lockedBalance = IFSushiVault(_vault).lockedUserBalanceDuring(account, week - 1);
-        uint256 lockedTotal = IFSushiVault(_vault).lockedTotalBalanceDuring(week - 1);
-
-        uint256 workingBalance = (balance * TOKENLESS_PRODUCTION) / 100;
-        if (lockedTotal > 0) {
-            workingBalance += (((supply * lockedBalance) / lockedTotal) * (100 - TOKENLESS_PRODUCTION)) / 100;
-        }
-
-        workingBalance = Math.min(workingBalance, balance);
-
-        uint256 prevBalance = workingBalanceOf[pid][account];
-        workingBalanceOf[pid][account] = workingBalance;
-
-        uint256 _workingSupply = workingSupply[pid] + workingBalance - prevBalance;
-        workingSupply[pid] = _workingSupply;
-
-        emit UpdateWorkingBalance(pid, account, workingBalance, _workingSupply);
-    }
-
-    function claimRewards(uint256 pid, address beneficiary) external {
-        _userCheckpoint(pid, msg.sender);
-
-        uint256 prevWeek = nextClaimableWeek[pid][msg.sender];
-        if (prevWeek == block.timestamp) return;
-        if (prevWeek == 0) {
-            prevWeek = startWeek.toTimestamp();
-        }
-
-        address _kitchen = kitchen;
-        IFSushiKitchen(_kitchen).checkpoint(pid);
-
-        // add week-by-week rewards until the last week
-        uint256 totalRewards;
-        uint256 from = prevWeek.toWeekNumber();
-        uint256 to = block.timestamp.toWeekNumber(); // exclusive last index
-        for (uint256 i; i < 512; ) {
-            uint256 week = from + i;
-            if (to <= week) break;
-            uint256 weight = IFSushiKitchen(_kitchen).relativeWeightAt(pid, week.toTimestamp());
-            uint256 rewards = (weeklyRewards[week] * weight * userPoints[pid][msg.sender][week]) /
-                points[pid][week] /
-                1e18;
-            totalRewards += rewards;
-
-            unchecked {
-                ++i;
-            }
-        }
-        nextClaimableWeek[pid][msg.sender] = to;
-
-        if (totalRewards > 0) {
-            claimedRewards[pid][msg.sender] += totalRewards;
-            IFSushi(fSushi).mint(beneficiary, totalRewards);
-
-            emit ClaimRewards(pid, msg.sender, beneficiary, totalRewards);
-        }
+        IFSushi(fSushi).mint(to, amount);
     }
 }
--- contracts/interfaces/ISousChef.sol
+++ contracts/interfaces/ISousChef.sol
@@ -3,25 +3,16 @@
 pragma solidity ^0.8.0;
 
 interface ISousChef {
-    error TooEarly();
+    error FSushiBillCreated();
     error InvalidPid();
     error InvalidFSushiVault();
     error InvalidFSushiKitchen();
-    error NoClaimableRewards();
+    error Forbidden();
 
     event UpdateFSushiVault(address indexed fSushiVault);
     event UpdateFSushiKitchen(address indexed fSushiKitchen);
-    event Deposit(uint256 indexed pid, uint256 amount, address indexed beneficiary);
-    event Withdraw(uint256 indexed pid, uint256 amount, address indexed beneficiary);
-    event Checkpoint(uint256 indexed pid);
-    event UserCheckpoint(uint256 indexed pid, address indexed account);
-    event UpdateWorkingBalance(
-        uint256 indexed pid,
-        address indexed account,
-        uint256 workingBalance,
-        uint256 workingSupply
-    );
-    event ClaimRewards(uint256 indexed pid, address indexed account, address indexed beneficiary, uint256 amount);
+    event CreateFSushiBill(uint256 indexed pid, address indexed bill);
+    event Checkpoint();
 
     function BONUS_MULTIPLIER() external view returns (uint256);
 
@@ -37,51 +28,25 @@ interface ISousChef {
 
     function kitchen() external view returns (address);
 
-    function weeklyRewards(uint256 time) external view returns (uint256);
+    function getFSushiBill(uint256 pid) external view returns (address);
 
-    function totalSupply(uint256 pid) external view returns (uint256);
+    function weeklyRewards(uint256 week) external view returns (uint256);
 
-    function workingSupply(uint256 pid) external view returns (uint256);
+    function lastCheckpoint() external view returns (uint256);
 
-    function points(uint256 pid, uint256 time) external view returns (uint256);
-
-    function lastCheckpoint(uint256 pid) external view returns (uint256 time);
-
-    function balanceOf(uint256 pid, address account) external view returns (uint256);
-
-    function workingBalanceOf(uint256 pid, address account) external view returns (uint256);
-
-    function userPoints(
-        uint256 pid,
-        address account,
-        uint256 time
-    ) external view returns (uint256);
-
-    function userLastCheckpoint(uint256 pid, address account) external view returns (uint256 time);
-
-    function claimedRewards(uint256 pid, address account) external view returns (uint256);
-
-    function nextClaimableWeek(uint256 pid, address account) external view returns (uint256);
+    function predictFSushiBillAddress(uint256 pid) external view returns (address bill);
 
     function updateFSushiVault(address _fSushiVault) external;
 
     function updateFSushiKitchen(address _fSushiKitchen) external;
 
-    function deposit(
+    function createFSushiBill(uint256 pid) external returns (address strategy);
+
+    function checkpoint() external;
+
+    function mintFSushi(
         uint256 pid,
-        uint256 amount,
-        address beneficiary
+        address to,
+        uint256 amount
     ) external;
-
-    function withdraw(
-        uint256 pid,
-        uint256 amount,
-        address beneficiary
-    ) external;
-
-    function claimRewards(uint256 pid, address beneficiary) external;
-
-    function checkpoint(uint256 pid) external;
-
-    function userCheckpoint(uint256 pid, address account) external;
 }
