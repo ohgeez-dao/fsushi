1beb4c6bea5c9657ee1948f7cb5d59ff74ea6ac9
LevX <team@levx.io>
2023-01-11 05:30:18 +0900

    Update FSushiCookV0

--- contracts/FSushiCookV0.sol
+++ contracts/FSushiCookV0.sol
@@ -2,6 +2,8 @@
 
 pragma solidity ^0.8.17;
 
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@sushiswap/core/contracts/uniswapv2/interfaces/IUniswapV2Pair.sol";
 import "./interfaces/IFlashStrategySushiSwapFactory.sol";
 import "./interfaces/IFlashStrategySushiSwap.sol";
 import "./interfaces/IFarmingLPTokenFactory.sol";
@@ -34,6 +36,8 @@ interface IFlashProtocol_ {
 }
 
 contract FSushiCookV0 {
+    using SafeERC20 for IERC20;
+
     address public immutable flashStrategyFactory;
     address public immutable flpTokenFactory;
     address public immutable sousChef;
@@ -58,7 +62,13 @@ contract FSushiCookV0 {
         bytes32 s
     ) public {
         address flpToken = IFarmingLPTokenFactory(flpTokenFactory).getFarmingLPToken(pid);
-        IFarmingLPToken(flpToken).depositSigned(amountLP, path0, path1, amountMin, address(this), deadline, v, r, s);
+        address lpToken = IFarmingLPToken(flpToken).lpToken();
+
+        IUniswapV2Pair(lpToken).permit(msg.sender, address(this), amountLP, deadline, v, r, s);
+        IERC20(lpToken).safeTransferFrom(msg.sender, address(this), amountLP);
+
+        IERC20(lpToken).approve(flpToken, amountLP);
+        IFarmingLPToken(flpToken).deposit(amountLP, path0, path1, amountMin, address(this), deadline);
 
         uint256 amountFLP = IFarmingLPToken(flpToken).balanceOf(address(this));
         address strategy = IFlashStrategySushiSwapFactory(flpTokenFactory).getFlashStrategySushiSwap(pid);
